9b782fd7325908d5c33efdeefb0b46278d42add2
mudgilsneha73-ui <mudgilsneha73@gmail.com>
2026-08-06 13:07:28 +0530

    Renamed a Python file

---
+++ 08_ Celsius_in_Fahrenheit.py
@@ -0,0 +1,7 @@
+# Write a program to read a temperature in Celsius and convert it to Fahrenheit.
+celsius = float(input("Enter the Temperature in Celcius :  "))
+
+fahrenheit = ( celsius * 9 / 5) + 32
+
+print(f" {celsius}°C in fahrenheit = {fahrenheit}°F ")
+
--- 08_ Celsius_into_Fahrenheit.py
+++
@@ -1,6 +0,0 @@
-celsius = float(input("Enter the Temperature in Celcius :  "))
-
-fahrenheit = ( celsius * 9 / 5) + 32
-
-print(f" {celsius} °C into fahrenheit = {fahrenheit}°F ")
-
