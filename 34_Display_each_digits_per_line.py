81aa0bd340afe4cc35c1c281df6ca2986f5b70ee
mudgilsneha73-ui <mudgilsneha73@gmail.com>
2026-08-10 21:42:06 +0530

    Modify 34 and 35

--- 34_Display_each_digits_per_line.py
+++ 34_Display_each_digits_per_line.py
@@ -1,17 +1,13 @@
-# #  Write a program to display all the digits of a number n (one per line)
+#   Write a program to display all the digits of a number n (one per line)
 
-# n = int(input("Enter the value of b n: "))
-# digit = 0
-# while n != 0:
-#     digit = n % 10 
-#     print(digit) 
-#     n = n // 10
+n = int(input("Enter the value of b n: "))
+
+digit = 0
+
+while n != 0:
+    digit = n % 10 
+
+    print(digit) 
     
-
-
-    # Display all digits of a number in original order.
-
-n = input("Enter the value of n: ")
-
-for digit in n:
-    print(digit)
+    n = n // 10
+    
--- 35_Sum_of_Digits.py
+++ 35_Sum_of_Digits.py
@@ -1,9 +1,12 @@
 # Write a program to find the sum of all digits of a number n.
 
 n = int(input("Enter the value of n: "))
+
 sum_digits = 0
+
 while n != 0:
     digit = n % 10
     sum_digits += digit
     n = n // 10
+    
 print("Sum of digits = ", sum_digits)
